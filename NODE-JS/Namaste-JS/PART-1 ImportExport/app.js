572f980999432bb21fa009d9b2fd20802dbfc20a
Divyanshu Tyagi <divyanshutyagi@Divyanshus-Mac-mini.local>
2024-12-19 07:27:39 +0530

    Import and export

---
+++ NODE-JS/Namaste-JS/PART-1 ImportExport/app.js
@@ -0,0 +1,10 @@
+// Enter file for the Project
+
+require('./calculateSum.js')
+
+
+var firstName = 'Divyanshu'
+var lastName = 'Tyagi'
+
+calculateSum(10, 20);
+console.log(firstName + ' ' + lastName)
---
+++ NODE-JS/Namaste-JS/PART-1 ImportExport/calculateSum.js
@@ -0,0 +1,6 @@
+console.log('Module is used to calculate the sum of 2 variable')
+
+export function calculateSum(a , b){
+    let sum = a + b;
+    console.log(sum);
+}
