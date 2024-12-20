eeb36bd581aea2978c2a7e2d35b19ea5af619cad
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-12-20 16:29:44 +0530

    Importing the json file using requrie function in Nested-export-withIndexFile folder

--- NODE-JS/Namaste-JS/PART-1 ImportExport/Nested-Export-withIndexFile/app.js
+++ NODE-JS/Namaste-JS/PART-1 ImportExport/Nested-Export-withIndexFile/app.js
@@ -6,3 +6,7 @@ let b = 4;
 
 console.log("the product of number is ", multiple(a, b));
 console.log("the sum of number is ", sum(a, b));
+
+// ! Importing the .json folder in app.json using require function
+let data = require("./data.json");
+console.log(data);
