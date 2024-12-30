621aeb1d49cde60ec52cb5eae860f3e67c6de386
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-12-30 16:11:16 +0530

    Closure

---
+++ JavaScript/Namaste/2. Closure/script.js
@@ -0,0 +1,10 @@
+function outerFunction(outerVariable) {
+  return function innerFunction(innerVariable) {
+    console.log(`Outer Variable: ${outerVariable}`);
+    console.log(`Inner Variable: ${innerVariable}`);
+  };
+}
+
+const closureFunction = outerFunction("Hello");
+closureFunction("World");
+closureFunction("Divyanshu");
