cdc310ac50b920bc81d1d1f138cc63505b39d430
Divyanshu Tyagi <divyanshu422@gmail.com>
2025-01-01 16:26:48 +0530

    Basic of array

---
+++ JavaScript/Units/ARRAYS/scriptBasic1.js
@@ -0,0 +1,32 @@
+//* Basic of array in javaScript
+
+/*
+    * Content:
+        - Creation of array: literal [] or Array constructor
+        - Length property: 'provides the value'
+        - typeof property: In javaScript, array are object
+
+
+*/
+
+// ! 1. Creation of array using array Constructor
+
+// const numbers = new Array(1, 2, 3, "Divyanshu");
+// console.log(numbers); // * [ 1, 2, 3, 'Divyanshu' ]
+
+// ! 2. Length Property ->
+
+// const arr = [2, 4, "Divyanshu"];
+// let firstName = "Divyanshu";
+// console.log(arr.length); //  3
+// console.log(firstName.length); // 9
+
+// ! 3. typeof operator:
+
+// console.log(typeof {}); // "object"
+// console.log(typeof []); // "object" (arrays are technically objects in JavaScript)
+
+// * Some take aways:
+// console.log(typeof null); // ! "object" (this is a historical quirk in JavaScript)
+// console.log(typeof NaN); // * "number" (NaN is still considered a type of number)
+// console.log(typeof undefined); // "undefined"
