56ae43555ef3a0eecbe25942823841f29a2487d0
Divyanshu Tyagi <divyanshutyagi@Divyanshus-Mac-mini.local>
2025-01-06 00:28:36 +0530

    Prototype Inheritance

--- JavaScript/Namaste/3. HOF/script.js
+++ JavaScript/Namaste/3. HOF/script.js
@@ -9,13 +9,13 @@ const diameter = (radius) => {
   return 2 * radius;
 };
 
-const genericFunction = function (logic, radius) {
+Array.prototype.genericFunction = function (logic) {
   const output = [];
-  for (let r of radius) {
+  for (let r of this) { // Here this means the radius on which generic function is called
     output.push(logic(r));
   }
   return output;
 };
 
-console.log(genericFunction(area, radius));
-console.log(genericFunction(diameter, radius));
+const result = radius.genericFunction(diameter);
+console.log(result)
