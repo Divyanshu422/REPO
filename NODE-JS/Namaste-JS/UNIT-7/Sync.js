98f4f4a7c7bd2ffbc6bf43245d635e2ee4142294
Divyanshu Tyagi <divyanshu422@gmail.com>
2025-02-07 17:28:47 +0530

    Adding the Synchronised code

---
+++ NODE-JS/Namaste-JS/UNIT-7/Sync.js
@@ -0,0 +1,12 @@
+console.log("Hello world");
+
+var a = 107469;
+var b = 31246;
+
+function multipleFn(x, y) {
+  const result = x * y;
+  return result;
+}
+
+var c = multipleFn(a, b);
+console.log("Multiplication result is : ", c);
