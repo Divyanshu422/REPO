8f3dcdcf6a3c7c0e4b83197543a5af2da8279767
Divyanshu Tyagi <divyanshutyagi@Divyanshus-Mac-mini.local>
2025-02-08 00:42:02 +0530

    async code

--- NODE-JS/Namaste-JS/UNIT-7/Readme.md
+++ NODE-JS/Namaste-JS/UNIT-7/Readme.md
@@ -1,1 +1,3 @@
 # The first code we have commited is `sync.js` file -> which reflect javaScript execute the synchronised code with in microSecon
+
+# The second code is `async.js`
---
+++ NODE-JS/Namaste-JS/UNIT-7/async.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const https = require("https");
+
+console.log("Hello world");
+
+var a = 100;
+var b = 6;
+
+https.get("https://dummyjson.com/products/1", res =>{
+    console.log("Data fetched successfully")
+})
+
+setTimeout((param)=>{
+    console.log("the setTimeout function executed in", param )
+}, 5000, 5000)
+
+fs.readFile("./file.txt", "utf8", (err,data) => {
+    console.log("file Data :", data);
+})
+
+
+function multiplyFn(x,y){
+    const result = x * y;
+    return result;
+}
+
+console.log("The result of multiplication is ", multiplyFn(a, b))
